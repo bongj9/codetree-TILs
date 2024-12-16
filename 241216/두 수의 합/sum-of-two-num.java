dad0ecc7d49327546c2fabb55ceaededbfb6296d
Bong J <114319597+bongj9@users.noreply.github.com>
2024-12-16 16:18:01 +0900

    Title: 두 수의 합, Time: 2012ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

--- 241216/두 수의 합/sum-of-two-num.java
+++ 241216/두 수의 합/sum-of-two-num.java
@@ -4,25 +4,24 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
         int n = sc.nextInt(); // 배열 크기
         int k = sc.nextInt(); // 목표 합
         int count = 0;
 
-        // 배열 입력 받기
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+        // HashMap으로 값 저장
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 1; i <= n; i++) {
+            map.put(i, sc.nextInt());
         }
 
-        // HashMap을 사용해 효율적으로 합 계산
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            int complement = k - arr[i]; // 현재 값과 더해 k가 되는 값
-            if (map.containsKey(complement)) {
-                count += map.get(complement); // 해당 값의 빈도수만큼 카운트
+        // 이중 for문으로 합 계산
+        for (int i = 1; i <= n; i++) {
+            for (int j = i + 1; j <= n; j++) { // i 이후의 값부터 탐색
+                if (map.get(i) + map.get(j) == k) { // 두 값의 합이 k와 같다면
+                    count++;
+                }
             }
-            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1); // 현재 값의 빈도수 업데이트
         }
 
         System.out.println(count);
