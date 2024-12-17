af8ba75a12698d8b8967495eea9ca934e0c6f9da
Bong J <114319597+bongj9@users.noreply.github.com>
2024-12-17 18:35:33 +0900

    Title: 순서를 바꾸었을 때 같은 단어 그룹화하기, Time: 124ms, Memory: 10MB, Status: Runtime Error - CodeTree

---
+++ 241217/순서를 바꾸었을 때 같은 단어 그룹화하기/group-same-word.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextInt();
+        HashMap<String, Integer> map = new HashMap<>();
+        for(int i = 0; i < n ; i++){
+            String input = sc.nextLine();
+            char [] charList = input.toCharArray();
+            Arrays.sort(charList);
+            String sortList = new String(charList);
+            
+            map.put(sortList, map.getOrDefault(sortList,0)+1);
+            //스트링을 쪼개서 char로 등록
+        }
+        int maxCount = Collections.max(map.values());
+        System.out.print(maxCount);
+
+    }
+}
