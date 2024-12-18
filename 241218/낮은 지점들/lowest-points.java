9ad4ccd0b6a25ed757ac86e541754ff8861dbf75
Bong J <114319597+bongj9@users.noreply.github.com>
2024-12-18 16:44:40 +0900

    Title: 낮은 지점들, Time: 891ms, Memory: 40MB, Status: Passed - CodeTree

--- 241218/낮은 지점들/lowest-points.java
+++ 241218/낮은 지점들/lowest-points.java
@@ -1,29 +1,30 @@
 import java.util.*;
-import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
-        int cnt = 0;
+        long cnt = 0; // 합계 변수를 long으로 선언
         LinkedHashMap<Integer, Integer> map = new LinkedHashMap<>();
-        for(int i = 0 ; i < n ; i ++ ){
+
+        for (int i = 0; i < n; i++) {
             int x = sc.nextInt();
             int y = sc.nextInt();
 
-            if(!map.containsKey(x)){
-                map.put(x,y);
-            }else if(map.get(x) >  y){
-                map.replace(x , y);
+            // 같은 x값이 없으면 추가, 있으면 더 작은 y로 업데이트
+            if (!map.containsKey(x)) {
+                map.put(x, y);
+            } else if (map.get(x) > y) {
+                map.put(x, y); // 더 작은 y로 업데이트
             }
-            
-            
         }
-        for(Map.Entry<Integer , Integer> entry : map.entrySet()){
-           cnt += entry.getValue();
-        }
-        System.out.println(cnt);
 
+        // 남아있는 점들의 y값 합산
+        for (int y : map.values()) {
+            cnt += y;
+        }
+
+        System.out.println(cnt);
     }
 }
