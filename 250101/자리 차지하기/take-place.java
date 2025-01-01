d16a2243adfe5bc835e3522ea3fbb1a491bc0162
Bong J <114319597+bongj9@users.noreply.github.com>
2025-01-01 23:18:34 +0900

    Title: 자리 차지하기, Time: 500ms, Memory: 28MB, Status: Passed - CodeTree

--- 250101/자리 차지하기/take-place.java
+++ 250101/자리 차지하기/take-place.java
@@ -9,7 +9,7 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         st = new StringTokenizer(br.readLine());
-        TreeSet<> set = new TreeSet<>();
+        TreeSet<Integer> set = new TreeSet<>();
         int [] arr = new int[100000];
         for(int i = 0; i< n ; i++){
             arr[i] = Integer.parseInt(st.nextToken());       
