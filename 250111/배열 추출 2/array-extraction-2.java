ecaf2cf217304d43e1345022847c19e80912f6dd
Bong J <114319597+bongj9@users.noreply.github.com>
2025-01-11 21:04:46 +0900

    Title: 배열 추출 2, Time: 951ms, Memory: 33MB, Status: Passed - CodeTree

---
+++ 250111/배열 추출 2/array-extraction-2.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+import java.util.PriorityQueue;
+
+class Pair implements Comparable<Pair> {
+    int absX, x;
+
+    public Pair(int absX, int x) {
+        this.absX = absX;
+        this.x = x;
+    }
+
+    @Override
+    public int compareTo(Pair p) {
+        if(this.absX != p.absX)
+            return this.absX - p.absX;   
+        else
+            return this.x - p.x;      
+    }
+}
+
+public class Main {
+    // 변수 선언:
+    public static int n;
+    
+    public static PriorityQueue<Pair> pq = new PriorityQueue<>();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+
+        for(int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+
+            if(x != 0)
+                pq.add(new Pair(Math.abs(x), x));
+         
+            else {
+                if(pq.isEmpty()) {
+                    System.out.println(0);
+                    continue;
+                }
+                System.out.println(pq.poll().x);
+            }
+        }
+    }
+}
